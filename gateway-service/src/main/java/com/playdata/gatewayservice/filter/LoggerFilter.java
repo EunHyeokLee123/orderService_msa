703067f630f6d0f29f79055cd93a5043ec9df62d
EunHyeok <secun77@naver.com>
2025-04-29 14:33:53 +0900

    게이트웨이 필터 생성 및 적용 기초, 우선순위 할당

--- gateway-service/src/main/java/com/playdata/gatewayservice/filter/LoggerFilter.java
+++ gateway-service/src/main/java/com/playdata/gatewayservice/filter/LoggerFilter.java
@@ -3,7 +3,9 @@ package com.playdata.gatewayservice.filter;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
@@ -11,41 +13,41 @@ import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
-public class GlobalFilter
-    // 제네릭에도 우리가 선언한 정적 클래스를 넣으면
-        extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+public class LoggerFilter  extends AbstractGatewayFilterFactory<LoggerFilter.Config> {
 
-    // Filter 객체가 생성되서 Bean으로 등록 될때, 부모 클래스의 생성자에게
-    // 이미 정적(static)으로 세팅된 특정 설정값을 전달합니다.
-    public GlobalFilter() {
+    public LoggerFilter() {
 
-        super(Config.class);
+        super(LoggerFilter.Config.class);
 
     }
 
+
     @Override
-    // override하는 메소드의 매개변수에 우리가 선언한 정적 클래스를 사용할 수 있음.
     public GatewayFilter apply(Config config) {
-        // 부모는 전달받은 설정값을 필터가 동작할 때 (apply가 호출될 때)
-        // 사용할 수 있도록 매개값으로 전달해줌.
-        return (exchange, chain) -> {
+        // 필터의 우선순위를 적용하고 싶다면 OrderedGatewayFilter를 직접 생성한다.
+        // 첫번째 매개값은 익명 객체 선언, 두번째 매개값으로 우선순위 상수를 넘깁니다.
+        // 만약 여러 필터에 순위를 매기고 싶다면 정수로 지정합니다. (낮은 숫자일수록 우선순위 높음)
+        return new OrderedGatewayFilter((exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             ServerHttpResponse response = exchange.getResponse();
 
-            log.info("Golbal Filter active! baseMessage = {}", config.getBaseMessage());
+            log.info("Logger Filter active! baseMessage = {}", config.getBaseMessage());
             if(config.isPreLogger()){
-                log.info("Global Pre Filter active! {}", response.getStatusCode());
+                log.info("Logger Pre Filter active! {}", response.getStatusCode());
             }
             return chain.filter(exchange).then(Mono.fromRunnable(()->{
                 if(config.isPostLogger()){
-                    log.info("Global Post Filter active!", response.getStatusCode());
+                    log.info("Logger Post Filter active!", response.getStatusCode());
                 }
             }));
-        };
+        },  // 필터의 우선순위를 넣는 Ordered의 enum값
+                Ordered.HIGHEST_PRECEDENCE);
     }
 
-    @Getter @Setter
-    @ToString @NoArgsConstructor
+    @Getter
+    @Setter
+    @ToString
+    @NoArgsConstructor
     @AllArgsConstructor
     public static class Config {
         // 필터 동작을 동적으로 변경하거나 설정하기 위해 사용함 (선택사항)
@@ -55,4 +57,5 @@ public class GlobalFilter
         private boolean postLogger;
 
     }
+
 }
